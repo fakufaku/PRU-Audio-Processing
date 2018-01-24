faaed18f7259658826969ae813746e6c7e883c32
Robin Scheibler <fakufaku@gmail.com>
2018-01-24 22:52:02 +0900

    Rezoomed the filter response plot.

--- utils/power_resp_CIC.py
+++ utils/power_resp_CIC.py
@@ -6,8 +6,9 @@ eps = sys.float_info.epsilon
 
 def P_hat(N, M, R, fs):
 
-    n_points = 100
-    freqs = np.linspace(0,1/M,n_points)
+    n_points = 1000
+    #freqs = np.linspace(0,(R / 2) * (1 / M),n_points)
+    freqs = np.linspace(0,(R / 2),n_points)
     P_est = np.zeros((n_points,1))
     P = P_est
 
@@ -15,7 +16,7 @@ def P_hat(N, M, R, fs):
         P_est[idx] = ( R*M * np.sin(np.pi*M*f) / (np.pi*M*f + eps) ) ** (2*N)
         P[idx] = (np.sin(np.pi*M*f)/(np.sin(np.pi*f/R)+eps))**(2*N)
 
-    freqs_hz = freqs * fs/R
+    freqs_hz = freqs * fs / R
 
     # drop DC value since NaN due to division by 0
     return P[1:], P_est[1:], freqs_hz[1:]  
@@ -26,7 +27,7 @@ def bit_width(N, M, R):
 
 if __name__ == "__main__":
 
-    N = 4
+    N = 3
     M = 1
     R = 16
     fs = 64 * 16e3
@@ -43,13 +44,19 @@ if __name__ == "__main__":
     # find fc
     fc = freqs_hz[np.argmin(abs(P_db+3))]  # -3dB point
 
+    fr = fs / R
+
     # visualize
     plt.ion()
     plt.figure()
-    plt.plot(freqs_hz, P_db,'b-')
-    plt.axvline(x=fc, color='r', linestyle='--')
-    plt.axvline(x=8000, color='g', linestyle='--')
+    plt.plot(freqs_hz / fr, P_db,'b-')
+    plt.axvline(x=fc / fr, color='r', linestyle='--')
+    plt.axvline(x=8000 / fr, color='g', linestyle='--')
     # plt.plot(freqs_hz, P_est_db)   # P_db essentially same as P_dB_est
+
+    for r in range(1, R//2):
+        plt.axvline(x=r - 8000 / fr, color='r', linestyle='-')
+        plt.axvline(x=r + 8000 / fr, color='r', linestyle='-')
 
     # add plot of f_c line
 
@@ -58,6 +65,9 @@ if __name__ == "__main__":
     plt.grid()
     plt.xlabel('Frequency [Hz]', fontsize=20)
     plt.ylabel('Attenuation [dB]', fontsize=20)
-    plt.ylim([-40,0])
+    plt.ylim([-120,0])
     plt.legend(['P', 'Cutoff', '8kHz'], fontsize=20)
+
+    plt.tight_layout(pad=0.1)
+
     plt.show()
